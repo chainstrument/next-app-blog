21bb2a531d691f0108ec93043c4d441cc6128f62
Chainstrument <minitutoriel@gmail.com>
2024-06-15 19:21:18 +0200

    test remote 2

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          My new title
+          My new title 2
         </h1>
 
         <p className={styles.description}>
