a9d4bfd7f4919d20658dc529a06b4635e0e21eea
Chainstrument <minitutoriel@gmail.com>
2024-06-16 12:33:22 +0200

    fixed:var undefined

--- nextjs-blog/pages/postslist.js
+++ nextjs-blog/pages/postslist.js
@@ -3,23 +3,52 @@ import { useEffect, useState } from "react";
 
 export default function Posts() {
     const [posts, setPosts] = useState([]); 
+    const [title, setTitle] = useState('');
 
     useEffect(() => {
 
         async function fetchPosts() {
             const res = await fetch('/api/posts');
             const data = await res.json();
+       
+            setPosts(data);
+
         }
         
-        setPosts(data);
-
         fetchPosts();
     
     }, [])
 
-    return 
-    <div>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch('/api/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title 
+            })
+        });
+    
+        const NewPost = await res.json();
+        setPosts([...posts, NewPost]);
+        setTitle('');
+
+    }
+
+    return <div>
         <h1>Posts</h1>
+            <form onSubmit={handleSubmit}> 
+                <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+                required
+                />
+            </form>
 
         <ul>
             {posts.map(post => (
@@ -27,10 +56,12 @@ export default function Posts() {
                     <h2>{post.title}</h2> 
                 </li>
             ))}
+           
         </ul>
 
-
+  
     </div>
 
 
 }
+ 
